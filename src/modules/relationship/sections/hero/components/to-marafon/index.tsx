2a1a2f8adc829bacc7c51c388074a95c688f4ca9
wayne <qleonnex@gmail.com>
2024-10-03 12:37:52 +0500

    init

--- src/modules/relationship/sections/hero/components/to-marafon/index.tsx
+++ src/modules/relationship/sections/hero/components/to-marafon/index.tsx
@@ -5,17 +5,24 @@ import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { Icon } from "@/components/ui/Icon";
 
+let windows = null;
+if (typeof window !== "undefined") {
+	windows = window;
+};
+
 export const ToMarafon = ({ variant, responsivable = false }: { variant: "light" | "dark", responsivable?: boolean }) => {
-	const { redirect } = useToMarafon();
-	const [size, setSize] = useState(window.innerWidth);
 	
-	useEffect(() => setSize(window.innerWidth), []);
+	
+	const { redirect } = useToMarafon();
+	const [size, setSize] = useState(windows?.innerWidth);
+	
+	useEffect(() => setSize(windows?.innerWidth), []);
 	
 	return (
 		<Button variant={variant} onClick={redirect}>
 			{!responsivable && "Присоединиться к марафону"}
-			{size > 1024 && responsivable && "Присоединиться к марафону"}
-			{size < 1024 && responsivable && <Icon path="/icons/arrow.svg" />}
+			{size! > 1024 && responsivable && "Присоединиться к марафону"}
+			{size! < 1024 && responsivable && <Icon path="/icons/arrow.svg" />}
 		</Button>
 	);
 };
