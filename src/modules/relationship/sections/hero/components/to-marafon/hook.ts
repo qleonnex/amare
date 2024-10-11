c1638fc2703cdaeaa418043815f62a381aed0cc0
Leonnex Q <qleonnex@gmail.com>
2024-10-12 03:29:05 +0500

    change link to tg

--- src/modules/relationship/sections/hero/components/to-marafon/hook.ts
+++ src/modules/relationship/sections/hero/components/to-marafon/hook.ts
@@ -4,7 +4,7 @@ export const useToMarafon = () => {
 	const router = useRouter()
 
 	function redirect() {
-		router.push("https://t.me/+Jp--HFmX8jRiYTI0")
+		router.push("https://t.me/+UuOCk0-Sj4AzMzY0")
 
 		if (typeof window.fbq === 'function') {
 			window.fbq('track', 'Lead')
