b3e92fd25027c120e127d010edbcdd7a78477369
qleonnex <qleonnex@gmail.com>
2024-10-19 15:28:13 +0500

    add new page

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-import path from 'path'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
-    return config;
+	eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
