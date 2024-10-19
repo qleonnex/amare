e516ed5901ad8e84af9a15d379b69f827a00f982
qleonnex <qleonnex@gmail.com>
2024-10-19 15:33:27 +0500

    add new page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	typescript: {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
     ignoreBuildErrors: true,
   },
 };
