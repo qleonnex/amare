ac2d88ecf2bd08c8c5611b65021ba4b5e2871fc9
qleonnex <qleonnex@gmail.com>
2024-11-06 15:39:47 +0500

    remove a date of marafon

--- src/modules/relationship/components/info/index.tsx
+++ src/modules/relationship/components/info/index.tsx
@@ -10,7 +10,6 @@ export const Info = ({ className = "" }: { className?: string }) => {
 				<h4>Марафон проходит в закрытой группе Telegram</h4>
 				<div className={cs.left_bottom}>
 					<p>C 12 по 14 ноября</p>
-					<p>Начало в 13ч МСК</p>
 				</div>
 			</div>
 		</Card>
