395b6039474bc13d6b85dcb15dcf893ecd9b0cda
wayne <qleonnex@gmail.com>
2025-10-25 19:25:23 +0500

    add: footer info

--- src/modules/reboot/components/sections/RebootFooter.tsx
+++ src/modules/reboot/components/sections/RebootFooter.tsx
@@ -24,6 +24,13 @@ export function RebootFooter() {
           </h4>
           <p className="text-black/60 max-md:text-sm">ИП Лазарева Екатерина</p>
         </div>
+          <div className="max-w-full opacity-60 text-center mt-16">
+              <div className="flex items-center justify-center gap-3.5 text-sm underline mb-3">
+                  <a href="/files/privacy-policy.docx" download>Политика конфиденциальности</a>
+                  <a href="/files/offer.docx" download>Договор оферты</a>
+              </div>
+              <p className="text-sm">Мы предоставляем возможность оплатить покупку онлайн непосредственно через интернет-магазин. К оплате принимаются пластиковые карты VISA, MasterCard и МИР, а также СБП, рассрочка и кредит. Прием оплат осуществляется при помощи платежного решения GetCourse (Get Модуль). Платежи, при этом данные карт и клиента передаются по защищенным каналам связи. Приём платежей происходит через защищённое безопасное соединение, используется SSL протокол.</p>
+          </div>
       </div>
     </div>
   );
